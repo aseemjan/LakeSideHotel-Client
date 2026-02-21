92ba85f6acc601fd33269c835b6bfc9120f845e2
Aseem <aseemjan7@gmail.com>
2026-02-21 16:51:17 +0530

    feat(homepage): implement Home UI with parallax, carousel & hotel section

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
+import MainHeader from '../layout/MainHeader'
+import RoomCarousel from '../common/RoomCarousel'
+import Parallax from '/Users/admin/Desktop/GitHub/LakeSide-Hotel/Client/src/components/common/Parallax.jsx'
+import HotelService from '/Users/admin/Desktop/GitHub/LakeSide-Hotel/Client/src/components/common/HotelService.jsx'
+
 
 const home = () => {
   return (
-    <div>
-      <h2>Welcome to The LakeSide Hotel</h2>
-    </div>
+    <section>
+        <MainHeader />
+        <section className='container'>
+          <RoomCarousel />
+          <Parallax />
+          <RoomCarousel />
+          <HotelService />
+          <Parallax />
+          <RoomCarousel />
+        </section>
+    </section>
   )
 }
 
---
+++ src/components/layout/MainHeader.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const MainHeader = () => {
+  return (
+    <header className='header-banner'>
+        <div className='overlay'></div>
+        <div className='animated-text overlay-content'>
+            <h1>Welcome to <span className='hotel-color'>LakeSide Hotel</span>
+            </h1>
+            <h4>Experience The Best Hospitality in Town</h4>
+        </div>
+    </header>
+  )
+}
+
+export default MainHeader
