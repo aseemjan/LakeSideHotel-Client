a7f5a5d92cf33fa6557267b667ef035fae66110d
Aseem <aseemjan7@gmail.com>
2026-03-27 08:08:28 +0530

    feat: implement Room Search component

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import MainHeader from '../layout/MainHeader'
 import RoomCarousel from '../common/RoomCarousel'
+import RoomSearch from '../common/RoomSearch'
 import Parallax from '/Users/admin/Desktop/GitHub/LakeSide-Hotel/Client/src/components/common/Parallax.jsx'
 import HotelService from '/Users/admin/Desktop/GitHub/LakeSide-Hotel/Client/src/components/common/HotelService.jsx'
+
 
 
 const home = () => {
@@ -10,6 +12,7 @@ const home = () => {
     <section>
         <MainHeader />
         <section className='container'>
+          <RoomSearch />
           <RoomCarousel />
           <Parallax />
           <RoomCarousel />
