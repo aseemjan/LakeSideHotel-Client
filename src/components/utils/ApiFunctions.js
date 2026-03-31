59237ef0fa2d8842dd493591e6fae5fb43b3c1f9
Aseem <aseemjan7@gmail.com>
2026-03-31 13:46:35 +0530

    feat: Implement frontend authentiction flow with context and JWT handling

--- src/components/utils/ApiFunctions.js
+++ src/components/utils/ApiFunctions.js
@@ -178,3 +178,40 @@ export async function getUserProfile(userId, token) {
 		throw error
 	}
 }
+
+/* This isthe function to delete a user */
+export async function deleteUser(userId) {
+	try {
+		const response = await api.delete(`/users/delete/${userId}`, {
+			headers: getHeader()
+		})
+		return response.data
+	} catch (error) {
+		return error.message
+	}
+}
+
+/* This is the function to get a single user */
+export async function getUser(userId, token) {
+	try {
+		const response = await api.get(`/users/${userId}`, {
+			headers: getHeader()
+		})
+		return response.data
+	} catch (error) {
+		throw error
+	}
+}
+
+/* This is the function to get user bookings by the user id */
+export async function getBookingsByUserId(userId, token) {
+	try {
+		const response = await api.get(`/bookings/user/${userId}/bookings`, {
+			headers: getHeader()
+		})
+		return response.data
+	} catch (error) {
+		console.error("Error fetching bookings:", error.message)
+		throw new Error("Failed to fetch bookings")
+	}
+}
