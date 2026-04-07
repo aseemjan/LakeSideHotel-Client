a70e998d88e4632bfe41b328ea417deb7b7cfd54
Aseem <aseemjan7@gmail.com>
2026-04-07 20:53:07 +0530

    feat : add log-in & out message in homepage

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -4,14 +4,24 @@ import RoomCarousel from '../common/RoomCarousel'
 import RoomSearch from '../common/RoomSearch'
 import Parallax from '/Users/admin/Desktop/GitHub/LakeSide-Hotel/Client/src/components/common/Parallax.jsx'
 import HotelService from '/Users/admin/Desktop/GitHub/LakeSide-Hotel/Client/src/components/common/HotelService.jsx'
-
+import { useLocation } from "react-router-dom"
+import { useAuth } from '../auth/AuthProvider'
 
 
 const home = () => {
+  const location = useLocation()
+
+	const message = location.state && location.state.message
+	const currentUser = localStorage.getItem("userId")
+
   return (
     <section>
+      {message && <p className="text-warning px-5">{message}</p>}
+			{currentUser && (
+				<h6 className="text-success text-center"> You are logged-In as {currentUser}</h6>
+			)}
         <MainHeader />
-        <section className='container'>
+        <div className='container'>
           <RoomSearch />
           <RoomCarousel />
           <Parallax />
@@ -19,7 +29,7 @@ const home = () => {
           <HotelService />
           <Parallax />
           <RoomCarousel />
-        </section>
+        </div>
     </section>
   )
 }
